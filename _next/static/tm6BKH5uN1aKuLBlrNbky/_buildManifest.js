8751bcced4dffb9683fd97fe199f80393dc71208
Vivek Juneja <vivekjuneja@gmail.com>
2023-05-01 20:21:35 +0200

    first commit

---
+++ _next/static/tm6BKH5uN1aKuLBlrNbky/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/1b55222842c4b5e4.css","static/chunks/pages/index-5fb9c66cad0cfc0a.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
